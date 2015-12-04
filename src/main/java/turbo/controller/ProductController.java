639b84620f085029978d959e32d035ad30f45ca1
Uy Nguyễn <leesan.itus@gmail.com>
2015-12-04 15:21:43 +0700

    remove context

--- src/main/java/turbo/controller/ProductController.java
+++ src/main/java/turbo/controller/ProductController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import turbo.POJO.Product;
-import turbo.common.ApplicationContextProvider;
+
 import turbo.service.ProductService;
 
 /**
